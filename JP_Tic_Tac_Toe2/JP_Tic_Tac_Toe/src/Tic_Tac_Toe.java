1042ce0a21ccb4bb32579d8dec9c87be847fdb80
Mila M <wanntofly8@gmail.com>
2015-10-24 15:06:01 -0400

    working progress tic tac toe

--- JP_Tic_Tac_Toe2/JP_Tic_Tac_Toe/src/Tic_Tac_Toe.java
+++ JP_Tic_Tac_Toe2/JP_Tic_Tac_Toe/src/Tic_Tac_Toe.java
@@ -16,25 +16,7 @@ public class Tic_Tac_Toe {
 		JLabel l = new JLabel("<html>Welcome to Tic Tac Toe Game<br>You will have X and computer will have O</html>", SwingConstants.CENTER);
 		int dialog = JOptionPane.showConfirmDialog(null, l);
 		createBoard(board, sb, tboard);
-		
-		//System.out.println(sb.getClass());
-		recursive(dialog, pos, tboard, sb, user, comp, board, counter);
-		
-//		if(dialog == JOptionPane.OK_OPTION){
-//			JLabel b = new JLabel("<html>Enter the position where you want to play<br>" + tboard + "</html>", SwingConstants.CENTER);
-//			pos = JOptionPane.showInputDialog(null, b);
-//			int index = Integer.parseInt(pos);
-//			//board[index-1] = x;
-//			//System.out.println(Arrays.toString(board));
-//			StringBuilder newBoard = updateBoard(board, sb, u, c, index, user=true, comp=false);
-//			JLabel n = new JLabel("<html>Your move is recorded, now computer will play" + newBoard + "</html>", SwingConstants.CENTER);
-//			int dialog2 = JOptionPane.showConfirmDialog(null, n);
-////			sb2 = compMove(board, sb2, u, c, index);
-//			newBoard = updateBoard(board, sb, u, c, index, user=false, comp=true);
-//			JLabel m = new JLabel("<html>Computer has played" + newBoard + "</html>", SwingConstants.CENTER);
-//			int dialog3 = JOptionPane.showConfirmDialog(null, m);
-//		}
-	
+		recursive(dialog, pos, tboard, sb, user, comp, board, counter);	
     }
 	
 	public static void createBoard(int[][] board, ArrayList sb, StringBuilder tboard) {
@@ -51,7 +33,7 @@ public class Tic_Tac_Toe {
 	}
 	
 	public static boolean isTaken(ArrayList sb, int rand) {
-		if (sb.get(rand) == "X") {
+		if ((sb.get(rand-1) == "X") || (sb.get(rand-1) == "O")) {
 			return false;
 		} else {
 		   return true;	
@@ -59,46 +41,53 @@ public class Tic_Tac_Toe {
 	}
 	
 	public static boolean isOneInColumnLeft(ArrayList sb, int[][] board, int counter) {
-		int temp;
+		//int temp;
 		int temp2;
-		for (int i = 0; i < board.length; i++) {
-			counter = 0;
-			for (int j = 0; j < board.length; j++) {
-				temp = board[i][j];
-				if (sb.get(temp) == "X") {
+		for (int i = 1; i <= board.length; i++) {
+			for (int j = 1; j <= board[i].length; j++) {
+				System.out.println("cell" + sb.get(board[i][j]));
+				if (sb.get(board[i][j]) == "X") {
+					
 					counter++;
-					if (counter == 2) {
-						for (int j2 = 0; j2 < 3; j2++) {
-							if(isTaken(sb, j2) == false) {
-								continue;
-							} else {
-								temp2 = board[i][j2];
-								sb.set(temp2, "O");
-							}
-						}
-					}
-				} 
+					System.out.println("counter" + counter);
+					return false;
+//					if (counter == 2) {
+//						for (int j2 = 1; j2 < 4; j2++) {
+//							if(isTaken(sb, j2) == true) {
+//								temp2 = board[i][j2];
+//								sb.set(temp2, "O");
+//								return false;
+//							} 
+//						}
+//					}
+				} else {
+					return true;
+				}
 		    }
 		}
-		return true;
+		return false;
 	}
 
 	public static StringBuilder updateBoard(int[][] board, ArrayList sb, int index, boolean user, boolean comp, int counter) {
 		int in = sb.indexOf(index);
 		if (user) {
 		  sb.set(in,"X");
+		  
 		} else if (comp) {
-			int rand = randInt(1, sb.size());
-			if (isTaken(sb, rand) == false) {
+			int rand;
+			while (true) {
+				//System.out.println("begin local" + localCount);
+				//if (localCount == 9) {
+				//	break;
+				//}
 				rand = randInt(1, sb.size());
-				isTaken(sb, rand);
+				System.out.println("rand" + rand);
+				if (isTaken(sb, rand) == true) {
+				  sb.set(rand-1, "O");
+				  break;
 				
-				if (!(isOneInColumnLeft(sb, board, counter))) {
-					sb.set(rand, "O");
-				}
-			} else {
-				if (!(isOneInColumnLeft(sb, board, counter))) {
-					sb.set(rand, "O");
+				} else {
+					continue;
 				}
 			}
 		}
@@ -130,7 +119,11 @@ public class Tic_Tac_Toe {
 			newBoard = updateBoard(board, sb, index, user=false, comp=true, counter);
 			JLabel m = new JLabel("<html>Computer has played" + newBoard + "</html>", SwingConstants.CENTER);
 			int dialog3 = JOptionPane.showConfirmDialog(null, m);
-			recursive(dialog, pos, newBoard, sb, user, comp, board, counter);
+			if (isOneInColumnLeft(sb, board, counter) == false) {
+		      System.out.println("comp won!");
+			} else {
+			  recursive(dialog, pos, newBoard, sb, user, comp, board, counter);
+			}
 		}
 	}
 }
